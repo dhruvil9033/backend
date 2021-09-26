b1eb9656608d97fffbd6ac1d13ad1ef549903b43
vasu-chapadia <chapadiavasu@gmail.com>
2021-09-26 18:49:21 +0530

    By V 26 Sept-6.48pm Created .env file

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello vvv')
+const express  = require('express')
+const app =  express();
+require('dotenv/config')
+const api = process.env.API_URL
+app.get(api+'/products', (req, res) => {
+    res.send('hello API !')
+})
+app.listen(3000, () => {
+    console.log('App running on port 3000')
+    console.log(api)
+
+})
