a43fc1067ab5b36b2608b82b6c06a6883d035459
vasu-chapadia <chapadiavasu@gmail.com>
2021-09-27 20:59:46 +0530

    By V 27Sept8.59pm - Post Product

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
-const express  = require('express')
-const app =  express();
-require('dotenv/config')
-const api = process.env.API_URL
+const express = require("express");
+const app = express();
+require("dotenv/config");
+
+const api = process.env.API_URL;
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 //Middleware
-app.use(express.json())
+app.use(express.json());
+app.use(morgan("tiny"));
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
+    }
+})
+const Product = mongoose.model('Product', productSchema)
 
 app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'fat milk',
-        image: 'some_url',
-    }
-    res.send(product)
-})
+  const product = {
+    id: 1,
+    name: "fat milk",
+    image: "some_url",
+  };
+  res.send(product);
+});
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body
-    console.log(newProduct)
-    res.send(newProduct)
-})
-app.listen(3000, () => {
-    console.log('App running on port 3000')
-    console.log(api)
+  const product = new Product({
+      name: req.body.name,
+      image: req.body.image,
+      countInStock: req.body.countInStock
+  })
 
-})
+  product.save().then((createdProduct => {
+      res.status(201).json(createdProduct)
+  })).catch((err)=>{
+      res.status(500).json({
+          error: err,
+          success: false
+      })
+  })
+});
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'milkapp-database'
+  })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+//   console.log(api);
+});
