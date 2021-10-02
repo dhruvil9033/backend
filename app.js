c065c88ca505af916b86db37e8b553d8a25879c2
vasu-chapadia <chapadiavasu@gmail.com>
2021-10-02 23:37:28 +0530

    By V 2Oct11.37pm - Routes and Schemas

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ require("dotenv/config");
 const mongoose = require("mongoose");
 const api = process.env.API_URL;
 const morgan = require("morgan");
-const productsRouter = require('./routers/products');
 
 //Middleware
 app.use(express.json());
 app.use(morgan("tiny"));
 
-//Routers
+//Routes
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
 
-app.use(`${api}/products`,productsRouter)
-const Product = require('./models/product')
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
+//Database
 mongoose
   .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
@@ -27,6 +33,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+//Server
 app.listen(3000, () => {
   console.log("App running on port 3000");
   //   console.log(api);
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-// const api = process.env.API_URL;
 const Product = require('../models/product')
 
 router.get(`/`, async (req, res) => {
