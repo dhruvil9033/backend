28065e07121e9ffa8bcb9521355247418f897b92
vasu-chapadia <chapadiavasu@gmail.com>
2021-10-07 22:36:49 +0530

    By V 7Oct10.36pm - auth and user permissions

--- app.js
+++ app.js
@@ -4,13 +4,33 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
-
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
 app.use(cors());
 app.options("*", cors());
 
 //middleware
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(authJwt()); //use braces whenever we call any function app.use
+app.use(errorHandler);
+// app.use((err, req, res, next)=>{
+//   console.log(err)
+//   if(err){
+//     if (err.name === "UnauthorizedError") {
+//           // jwt authentication error
+//           return res.status(401).json({ message: "The user is not authorized" });
+//         }
+      
+//         if (err.name === "ValidationError") {
+//           //  validation error
+//           return res.status(401).json({ message: err });
+//         }
+//     res.status(500).json({
+//       message: err
+//     })
+//   }
+// })
 
 //Routes
 const categoriesRoutes = require("./routes/categories");
